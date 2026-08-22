438806e13fb24192eda54fb1497c07d6d35a5610
nokoraalatesu01 <alatesu01@gmail.com>
2026-08-23 01:34:44 +0700

    Bai Tap Tu Lam: Quang Cao Ban Tui Xach

---
+++ BagAD/BagAD/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using BagAD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BagAD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    ID = 1,
+                    Name = "Product 1",
+                    Price = 500000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product1.jpg"
+                },
+
+                new Product
+                {
+                    ID = 2,
+                    Name = "Product 2",
+                    Price = 700000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product2.jpg"
+                },
+
+                new Product
+                {
+                    ID = 3,
+                    Name = "Product 3",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product3.jpeg"
+                },
+
+                new Product
+                {
+                    ID = 4,
+                    Name = "Product 4",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product4.jpg"
+                }
+            };
+            return View(products);
+        }
+    }
+}
---
+++ BagAD/BagAD/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BagAD.Models
+{
+    public class Product : Controller
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Image { get; set; }
+    }
+}
