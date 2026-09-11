78b80afa24d54ae63146b498dd763dacd7190b56
nokoraalatesu01 <alatesu01@gmail.com>
2026-09-11 11:51:41 +0700

    Bài Thực Hành 3 - bài 1:Danh sách Sách

---
+++ SachMVCLab03/SachMVCLab03/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SachMVCLab03.Models;
+
+namespace SachMVCLab03.Controllers
+{
+    public class BookController : Controller
+    {
+        /// <summary>
+        /// Authors : Nguyễn Đức Trường
+        /// Lớp IT2
+        /// MSSV 241230872
+        /// </summary>
+        protected Book book = new Book();
+        public IActionResult Index()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            var books = book.GetBooksList();
+            return View(books);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            Book model = new Book();
+            return View(model);
+        }
+
+        public IActionResult Edit(int id) 
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            Book model = book.GetBookByID(id);
+            return View(model);
+        }
+    }
+}
---
+++ SachMVCLab03/SachMVCLab03/Models/Book.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SachMVCLab03.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public int TotalPage { get; set; }
+        public string Sumary { get; set; }
+
+        public List<Book> GetBooksList()
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Slam Dunk",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "/images/products/b1.png",
+                    Price = 30000,
+                    Sumary = "",
+                    TotalPage = 50
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Conan",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "/images/products/b2.png",
+                    Price = 25000,
+                    Sumary = "",
+                    TotalPage = 50
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Title = "One Piece",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "/images/products/b3.png",
+                    Price = 30000,
+                    Sumary = "",
+                    TotalPage = 50
+                },
+                new Book()
+                {
+                    Id = 4,
+                    Title = "Wing books",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "/images/products/b4.png",
+                    Price = 300000,
+                    Sumary = "",
+                    TotalPage = 500
+                },
+            };
+            return books;
+        }
+
+        public Book GetBookByID(int id)
+        {
+            Book book = this.GetBooksList().FirstOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public List<SelectListItem> Authors { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value="1", Text = "Takehiko Inoue"},
+            new SelectListItem {Value="2", Text = "Gosho Aoyama"},
+            new SelectListItem {Value="3", Text = "Atchaikenai Yatsu"},
+            new SelectListItem {Value="4", Text = "Gin Shirakawa"},
+        };
+
+        public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "1", Text = "Truyện Tranh"},
+            new SelectListItem {Value = "2", Text = "Trinh thám"},
+            new SelectListItem {Value = "3", Text = "Siêu nhiên"},
+            new SelectListItem {Value = "4", Text = "Tiểu thuyết"},
+        };
+    }
+}
